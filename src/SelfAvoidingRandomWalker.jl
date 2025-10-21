67c77f114c0f6c0c41ce66acb77e71fc360cc16e
Joroks <johannes@roksvaag.com>
2025-10-21 09:34:05 +0200

    fixup

--- src/SelfAvoidingRandomWalker.jl
+++ src/SelfAvoidingRandomWalker.jl
@@ -6,9 +6,6 @@ using ProgressMeter
 using Distributions
 using UnicodePlots
 using Printf
-using Pipe: @pipe
-import ModernGL, GLFW
-import CImGui as ig
 
 const Vector3 = SVector{3, Float64}
 const Matrix3x3 = SMatrix{3, 3, Float64, 9}
